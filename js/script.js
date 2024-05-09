43e3b85d57b83edb81270c6f0e2d84a11eb3edb1
matthew aboyade <aboyadematthew@gmail.com>
2024-05-08 19:01:57 -0700

    add flash card divs creation

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $("button").click(function () {
+     let index = $("button").index(this);
+      $(".toggle").hide();
+      $(".toggle").eq(index).show();
+      $(".toggle").eq(index).find(".word-container").toggle();
+    });
+  });
+
+
+//   $(document).ready(function () {
+//     $(".button-div").click(function () {
+//      let index = $(".button-div").index(this);
+//       $(".toggle").hide();
+//       $("..word-container").hide();
+//       $(".toggle").eq(index).show();
+//       $("..word-container").eq(index).show();
+//       $(".toggle").eq(index).find(".word-container").toggle();
+//       $(".word-container").eq(index).find(".toggle").toggle();
+//     });
+//   });
